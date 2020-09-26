25f19ba74243813607014322457c1dd105bfed4c
Jacob-Bordelon <jab145@email.latech.edu>
2020-09-26 10:54:49 -0500

    first

---
+++ Assets/Scripts/ChangeName.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeName : MonoBehaviour
+{
+    public TextMeshProUGUI NameText;
+    string[] names = {"Jacob","Lori","Chris","Aunt Tina"};
+    int i=0;
+
+    private void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")){
+            if (i < names.Length){
+                NameText.text = names[i];
+                i++;
+
+            }
+        }
+
+
+
+    }
+}
